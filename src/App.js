8c96d96d4ebd68063c2851a26ac6ab8f3597772f
Sasha Fu <mariafu0801@gmail.com>
2021-03-18 11:58:38 +0800

    create Api and success connected

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
 import styles from "./App.module.css";
 
 class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
